b9d07e34b9519dc252b3be8857c5066a120c201d
Petr Hatoň <petrhaton@Petr--MacBook-Pro.local>
2024-11-27 14:03:43 +0100

    Oprava entrypointu z duvodu ca trustu

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
+mkdir -p /opt/app-root/ca-trust
+cp /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem /opt/app-root/ca-trust/
 if [ -d /opt/custom-certificates ]; then
   echo "Trusting custom certificates from /opt/custom-certificates."
-  mkdir -p /opt/app-root/ca-trust
-  cp /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem /opt/app-root/ca-trust/
   for cert_file in /opt/custom-certificates/*; do
     if [ -f "${cert_file}" ] && grep -q "BEGIN CERTIFICATE" "${cert_file}"; then
       cat "${cert_file}" >> /opt/app-root/ca-trust/tls-ca-bundle.pem
     fi
   done
-  export NODE_EXTRA_CA_CERTS=/opt/app-root/ca-trust/tls-ca-bundle.pem
 fi
+export NODE_EXTRA_CA_CERTS=/opt/app-root/ca-trust/tls-ca-bundle.pem
 
 if [ "$#" -gt 0 ]; then
   # Got started with arguments
